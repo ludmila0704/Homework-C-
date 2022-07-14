1455c6d54399dfe77bb9a7c92bf47ac3beaf0f22
Ludmila <ludmila0704@yandex.ru>
2022-07-14 22:22:07 +0300

    Homework2 task13

--- Homework2/task13/Program.cs
+++ Homework2/task13/Program.cs
@@ -6,23 +6,23 @@
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int ThirthDigit (int number)
-{ int a =0;
-    if ((number>0&&number<100)||(number<0&&number>-99)) return -1;
+int ThirthDigit(int number)
+{
+    int a = 0;
+    if ((number >=0 && number < 100) || (number < 0 && number > -99)) return -1;
     else
     {
-        while (number>100)
+        number = Math.Abs(number);
+        while (number > 100)
         {
-           a = Math.Abs(number%10);
-        //return a;
-        number=number/10;
-        } return a;
-    }//return  0;
+            a = number % 10;
+            number = number / 10;
+        }
+        return a;
+    }
 }
 
-int result =  ThirthDigit(num);
-//if (result!=-1) 
+int result = ThirthDigit(num);
+if (result == -1) Console.WriteLine($"Третьей цифры в числе {num}  нет.");
+else Console.WriteLine($"{num}  -> третья цифра {result}");
 
-    Console.WriteLine($"{num}  ->  {result}");
-
-//else ($"Третьей цифры в числе {num}  нет.");
